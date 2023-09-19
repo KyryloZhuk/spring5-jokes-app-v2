98aaf3b8a059b874df705e2dd06f8c53600716d8
KyryloZhuk <kyrylo.zhuk@intellecteu.com>
2023-09-19 19:11:10 +0200

    more small refactoring

--- src/main/java/com/kjcode/chucknorrisjokes/controller/JokesController.java
+++ src/main/java/com/kjcode/chucknorrisjokes/controller/JokesController.java
@@ -1,27 +1,27 @@
 package com.kjcode.chucknorrisjokes.controller;
 
 import com.kjcode.chucknorrisjokes.service.JokesService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "joke")
-public class ChuckNorrisJokesController {
+public class JokesController {
 
     private JokesService jokesService;
 
-    @Autowired
-    public ChuckNorrisJokesController(JokesService jokesService) {
+    public JokesController(JokesService jokesService) {
         this.jokesService = jokesService;
     }
 
     @GetMapping
-    public ResponseEntity<Object> getRandomJoke(){
-        return new ResponseEntity(jokesService.getJoke(), HttpStatus.OK);
+    public String getRandomJoke(Model model){
+
+        model.addAttribute("joke", jokesService.getJoke());
+
+        return "index";
     }
 }
 
